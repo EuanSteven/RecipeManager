8ee9b373a77448c8a79eada0a75a60b87033dc67
EuanSteven <23006327@uhi.ac.uk>
2024-09-10 22:53:26 +0100

    CREATE

--- backup/index.php
+++ backup/index.php
@@ -21,15 +21,6 @@
   <link rel="apple-touch-icon" href="/assets/icons/icon.png">
 
   <link rel="stylesheet" href="/assets/css/main.css">
-  <style>
-    .sort-indicator[data-order="asc"] .desc-arrow,
-    .sort-indicator[data-order="desc"] .asc-arrow {
-      fill: #E0E0E0;
-    }
-    .option.active {
-      font-weight: bold;
-    }
-  </style>
 </head>
 
 <body>
@@ -63,7 +54,7 @@
   <div class="search-container">
     <div class="search">
       <searchlabel>
-        <input type="text" id=searchInput class="searchTerm" placeholder="Search">
+        <input type="text" class="searchTerm" placeholder="Search">
       </searchlabel>
     </div>
 
@@ -92,6 +83,33 @@
             </label><br>
           </form>
 
+          <span class="filteroption">Foods
+              <svg width="30" height="31" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M21 30.2399C18.5 30.2399 16.375 29.3649 14.625 27.6149C12.875 25.8649 12 23.7399 12 21.2399C12 18.7399 12.875 16.6149 14.625 14.8649C16.375 13.1149 18.5 12.2399 21 12.2399C23.5 12.2399 25.625 13.1149 27.375 14.8649C29.125 16.6149 30 18.7399 30 21.2399C30 23.7399 29.125 25.8649 27.375 27.6149C25.625 29.3649 23.5 30.2399 21 30.2399ZM21 27.2399C22.65 27.2399 24.0625 26.6524 25.2375 25.4774C26.4125 24.3024 27 22.8899 27 21.2399C27 19.5899 26.4125 18.1774 25.2375 17.0024C24.0625 15.8274 22.65 15.2399 21 15.2399C19.35 15.2399 17.9375 15.8274 16.7625 17.0024C15.5875 18.1774 15 19.5899 15 21.2399C15 22.8899 15.5875 24.3024 16.7625 25.4774C17.9375 26.6524 19.35 27.2399 21 27.2399ZM0 27.2399V12.1649L3.225 4.73993H1.5V0.239929H15V4.73993H13.275L15.75 10.4399C15.275 10.6899 14.825 10.9524 14.4 11.2274C13.975 11.5024 13.575 11.8149 13.2 12.1649L10.05 4.73993H6.45L3 12.8399V24.2399H9.375C9.5 24.7649 9.66875 25.2837 9.88125 25.7962C10.0938 26.3087 10.35 26.7899 10.65 27.2399H0ZM21 10.7399C19.95 10.7399 19.0625 10.3774 18.3375 9.65243C17.6125 8.92743 17.25 8.03993 17.25 6.98993C17.25 5.93993 17.6125 5.05243 18.3375 4.32743C19.0625 3.60243 19.95 3.23993 21 3.23993V10.7399C21 9.68993 21.3625 8.80243 22.0875 8.07743C22.8125 7.35243 23.7 6.98993 24.75 6.98993C25.8 6.98993 26.6875 7.35243 27.4125 8.07743C28.1375 8.80243 28.5 9.68993 28.5 10.7399H21Z" fill="#A19A9A"/>
+              </svg>
+          </span>
+
+          <div class="filtersearch">
+            <filtersearchlabel>
+              <input type="text" class="filtersearchTerm" placeholder="Search">
+            </filtersearchlabel>
+          </div>
+
+          <form class="categoryoptions" action="/action_page.php">
+            <label for="beef">
+              <input type="checkbox" style="float: left;" id="Beef" name="beef" value="Beef">
+              <div style="overflow: hidden; padding: 0px 0px 0px 5px;">Beef</div>
+            </label><br>
+            <label for="chicken">
+              <input type="checkbox" style="float: left;" id="Chicken" name="chicken" value="Chicken">
+              <div style="overflow: hidden; padding: 0px 0px 0px 5px;">Chicken</div>
+            </label><br>
+            <label for="rice">
+              <input type="checkbox" style="float: left;" id="Rice" name="rice" value="Rice">
+              <div style="overflow: hidden; padding: 0px 0px 0px 5px;">Rice</div>
+            </label><br>
+          </form>
+
           <span class="filteroption">Dietary
               <svg width="24" height="24" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M3.954 24.0453C2.71837 22.8094 1.75047 21.3813 1.05028 19.761C0.350094 18.1406 0 16.4654 0 14.7352C0 13.005 0.3295 11.2954 0.9885 9.60638C1.6475 7.91738 2.71837 6.33137 4.20112 4.84835C5.16216 3.88713 6.34974 3.06323 7.76384 2.37664C9.17794 1.69006 10.8529 1.14765 12.7887 0.749434C14.7245 0.351215 16.9349 0.11091 19.4199 0.0285197C21.9049 -0.0538705 24.685 0.0422514 27.7604 0.316885C27.98 3.22801 28.0487 5.90569 27.9663 8.34993C27.8839 10.7942 27.6574 12.9981 27.2867 14.9617C26.916 16.9254 26.3943 18.6418 25.7216 20.1111C25.0489 21.5804 24.2182 22.8094 23.2297 23.7981C21.7745 25.2537 20.2299 26.3179 18.5961 26.9907C16.9624 27.6636 15.2943 28 13.5919 28C11.8071 28 10.0635 27.6498 8.36106 26.9495C6.65864 26.2492 5.18962 25.2811 3.954 24.0453ZM8.567 23.3861C9.36329 23.853 10.1802 24.1895 11.0177 24.3954C11.8551 24.6014 12.7132 24.7044 13.5919 24.7044C14.855 24.7044 16.1043 24.4504 17.3399 23.9423C18.5756 23.4342 19.7563 22.6172 20.8821 21.4912C21.3763 20.9968 21.8774 20.3034 22.3854 19.4108C22.8934 18.5183 23.3327 17.3511 23.7034 15.9092C24.0741 14.4674 24.3555 12.7235 24.5477 10.6775C24.7399 8.63143 24.7674 6.19405 24.6301 3.36532C23.2847 3.3104 21.7676 3.2898 20.0789 3.30353C18.3902 3.31726 16.7084 3.44771 15.0334 3.69488C13.3585 3.94205 11.7659 4.34027 10.2557 4.88954C8.74548 5.43881 7.50985 6.19405 6.54881 7.15527C5.31319 8.39113 4.46198 9.61325 3.99519 10.8216C3.52839 12.03 3.295 13.1972 3.295 14.3232C3.295 15.9436 3.6039 17.3648 4.22172 18.5869C4.83953 19.809 5.38183 20.6673 5.84862 21.1616C7.00187 18.9645 8.52581 16.8567 10.4204 14.8382C12.3151 12.8196 14.5255 11.1649 17.0516 9.87415C15.0746 11.6043 13.3516 13.5611 11.8826 15.7445C10.4136 17.9278 9.30837 20.475 8.567 23.3861Z" fill="#A19A9A"/>
@@ -116,23 +134,22 @@
       </button>
     </div>
 
-  <div class="dropdown">
-    <button class="icon-button">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#a19a9a"><path d="M400-240v-80h160v80H400ZM240-440v-80h480v80H240ZM120-640v-80h720v80H120Z"/></svg>
-      <div class="dropdown-content">
-        <span class="sortby">Sort By
-          <svg width="10" height="10" viewBox="0 0 18 22" fill="none" xmlns="http://www.w3.org/2000/svg" class="sort-indicator" data-order="asc">
-            <path d="M5.25 2.86875L1.05 7.06875L0 6L6 0L12 6L10.95 7.06875L6.75 2.86875V12L5.25 12L5.25 2.86875Z" fill="#A19A9A" class="asc-arrow"/>
-            <path d="M11.25 19.1313L7.05 14.9313L6 16L12 22L18 16L16.95 14.9312L12.75 19.1313L12.75 10H11.25L11.25 19.1313Z" fill="#A19A9A" class="desc-arrow"/>
-          </svg>
-        </span>
-        <span class="option" data-sort="name"><a href="#">Recipe Name</a></span>
-        <span class="option" data-sort="time"><a href="#">Cooking Time</a></span>
-        <span class="option" data-sort="size"><a href="#">Serving Size</a></span>
-      </div>
-    </button>
-  </div>
-
+    <div class="dropdown">
+      <button class="icon-button">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#a19a9a"><path d="M400-240v-80h160v80H400ZM240-440v-80h480v80H240ZM120-640v-80h720v80H120Z"/></svg>
+        <div class="dropdown-content">
+          <span class="sortby">Sort By
+            <svg width="10" height="10" viewBox="0 0 18 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M5.25 2.86875L1.05 7.06875L0 6L6 0L12 6L10.95 7.06875L6.75 2.86875V12L5.25 12L5.25 2.86875Z" fill="#A19A9A"/>
+              <path d="M11.25 19.1313L7.05 14.9313L6 16L12 22L18 16L16.95 14.9312L12.75 19.1313L12.75 10H11.25L11.25 19.1313Z" fill="#A19A9A"/>
+            </svg>
+          </span>
+          <span class="option"><a href="#">Recipe Name</a></span>
+          <span class="option"><a href="#">Cooking Time</a></span>
+          <span class="option"><a href="#">Size<a></span>
+        </div>
+      </button>
+    </div>
   </div>
   <?php
   // Database connection
@@ -184,14 +201,7 @@
                   </a>
                   <div class="recipe-details">
                       <div class="recipe-header">
-                          <span class="category">
-                            <?php 
-                            echo $category; 
-                            if (!empty($dietLabelString)) {
-                                echo " | " . $dietLabelString;
-                            }
-                            ?>
-                          </span>
+                          <span class="category"><?php echo $category; ?> | <?php echo $dietLabelString; ?></span>
                       </div>
                       <h3 class="recipe-title">
                           <a class="recipelink" href="/pages/recipe.php?id=<?php echo $recipeId; ?>">
@@ -227,198 +237,33 @@
 
   <div class="paginationcenter">
     <div class="pagination">
-        <?php if ($page > 1): ?>
-            <a href="?page=1">
-                <svg width="24" height="12" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9.65375 19.3075L0 9.65375L9.65375 0L11.073 1.41925L2.83825 9.65375L11.073 17.8883L9.65375 19.3075Z" fill="#A19A9A"/>
-                    <path d="M21.65375 19.3075L12 9.65375L21.65375 0L23.073 1.41925L14.83825 9.65375L23.073 17.8883L21.65375 19.3075Z" fill="#A19A9A"/>
-                </svg>
-            </a>
-            <a href="?page=<?php echo $page - 1; ?>">
-                <svg width="12" height="12" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9.65375 19.3075L0 9.65375L9.65375 0L11.073 1.41925L2.83825 9.65375L11.073 17.8883L9.65375 19.3075Z" fill="#A19A9A"/>
-                </svg>
-            </a>
-        <?php endif; ?>
-
-        <?php
-        $startPage = max(1, $page - 1);
-        $endPage = min($totalPages, $page + 1);
-        for ($i = $startPage; $i <= $endPage; $i++):
-        ?>
-            <a href="?page=<?php echo $i; ?>" <?php echo ($i == $page) ? 'class="active"' : ''; ?>><?php echo $i; ?></a>
-        <?php endfor; ?>
-
-        <?php if ($page < $totalPages): ?>
-            <a href="?page=<?php echo $page + 1; ?>">
-                <svg width="12" height="12" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1.41925 19.3075L11.073 9.65375L1.41925 0L-1.90735e-06 1.41925L8.23475 9.65375L-1.90735e-06 17.8883L1.41925 19.3075Z" fill="#A19A9A"/>
-                </svg>
-            </a>
-            <a href="?page=<?php echo $totalPages; ?>">
-                <svg width="24" height="12" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M2.34625 0L12 9.65375L2.34625 19.3075L0.927 17.8883L9.16175 9.65375L0.927 1.41925L2.34625 0Z" fill="#A19A9A"/>
-                    <path d="M14.34625 0L24 9.65375L14.34625 19.3075L12.927 17.8883L21.16175 9.65375L12.927 1.41925L14.34625 0Z" fill="#A19A9A"/>
-                </svg>
-            </a>
-        <?php endif; ?>
+      <a href="#">
+        <svg width="24" height="12" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M9.65375 19.3075L0 9.65375L9.65375 0L11.073 1.41925L2.83825 9.65375L11.073 17.8883L9.65375 19.3075Z" fill="#A19A9A"/>
+          <path d="M21.65375 19.3075L12 9.65375L21.65375 0L23.073 1.41925L14.83825 9.65375L23.073 17.8883L21.65375 19.3075Z" fill="#A19A9A"/>
+        </svg>
+      </a>
+      <a href="#">
+        <svg width="12" height="12" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M9.65375 19.3075L0 9.65375L9.65375 0L11.073 1.41925L2.83825 9.65375L11.073 17.8883L9.65375 19.3075Z" fill="#A19A9A"/>
+        </svg>
+        </a>
+      <a href="#" class="active">1</a>
+      <a href="#">2</a>
+      <a href="#">3</a>
+      <a href="#">
+        <svg width="12" height="12" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.41925 19.3075L11.073 9.65375L1.41925 0L-1.90735e-06 1.41925L8.23475 9.65375L-1.90735e-06 17.8883L1.41925 19.3075Z" fill="#A19A9A"/>
+        </svg>
+      </a>
+      <a href="#">
+        <svg width="24" height="12" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M2.34625 0L12 9.65375L2.34625 19.3075L0.927 17.8883L9.16175 9.65375L0.927 1.41925L2.34625 0Z" fill="#A19A9A"/>
+          <path d="M14.34625 0L24 9.65375L14.34625 19.3075L12.927 17.8883L21.16175 9.65375L12.927 1.41925L14.34625 0Z" fill="#A19A9A"/>
+        </svg>
+      </a>
     </div>
-</div>
-
-  <script>
-  document.addEventListener('DOMContentLoaded', function() {
-    const searchInput = document.getElementById('searchInput');
-    const recipeList = document.querySelector('.recipe-list');
-    const pagination = document.querySelector('.pagination');
-    let currentPage = 1;
-    let recipesPerPage = 10;
-
-    // Function to perform the search
-    function searchRecipes() {
-        const filter = searchInput.value.toUpperCase();
-        const recipes = document.getElementsByClassName('recipe');
-        let visibleCount = 0;
-
-        for (let i = 0; i < recipes.length; i++) {
-            const recipe = recipes[i];
-            const title = recipe.getElementsByClassName('recipe-title')[0];
-            const category = recipe.getElementsByClassName('category')[0];
-            const txtValue = title.textContent || title.innerText;
-            const categoryValue = category.textContent || category.innerText;
-
-            if (txtValue.toUpperCase().indexOf(filter) > -1 || categoryValue.toUpperCase().indexOf(filter) > -1) {
-                recipe.style.display = "";
-                visibleCount++;
-            } else {
-                recipe.style.display = "none";
-            }
-        }
-
-        updatePagination(visibleCount);
-    }
-
-    // Function to sort recipes
-    function sortRecipes(sortBy, order) {
-        const recipes = Array.from(recipeList.getElementsByClassName('recipe'));
-
-        recipes.sort((a, b) => {
-            let aValue, bValue;
-
-            switch (sortBy) {
-                case 'name':
-                    aValue = a.querySelector('.recipe-title').textContent.trim().toLowerCase();
-                    bValue = b.querySelector('.recipe-title').textContent.trim().toLowerCase();
-                    break;
-                case 'time':
-                    aValue = parseInt(a.querySelector('.cooking-time').textContent);
-                    bValue = parseInt(b.querySelector('.cooking-time').textContent);
-                    break;
-                case 'size':
-                    aValue = parseInt(a.querySelector('.serving-size').textContent);
-                    bValue = parseInt(b.querySelector('.serving-size').textContent);
-                    break;
-            }
-
-            if (order === 'asc') {
-                return aValue > bValue ? 1 : -1;
-            } else {
-                return aValue < bValue ? 1 : -1;
-            }
-        });
-
-        recipes.forEach(recipe => recipeList.appendChild(recipe));
-        updatePagination(recipes.length);
-    }
-
-    // Function to update pagination
-    function updatePagination(totalRecipes) {
-        const totalPages = Math.ceil(totalRecipes / recipesPerPage);
-        let paginationHTML = '';
-
-        if (currentPage > 1) {
-            paginationHTML += `<a href="#" data-page="1">&laquo;</a>`;
-            paginationHTML += `<a href="#" data-page="${currentPage - 1}">&lsaquo;</a>`;
-        }
-
-        const startPage = Math.max(1, currentPage - 1);
-        const endPage = Math.min(totalPages, currentPage + 1);
-
-        for (let i = startPage; i <= endPage; i++) {
-            paginationHTML += `<a href="#" data-page="${i}" ${i === currentPage ? 'class="active"' : ''}>${i}</a>`;
-        }
-
-        if (currentPage < totalPages) {
-            paginationHTML += `<a href="#" data-page="${currentPage + 1}">&rsaquo;</a>`;
-            paginationHTML += `<a href="#" data-page="${totalPages}">&raquo;</a>`;
-        }
-
-        pagination.innerHTML = paginationHTML;
-        addPaginationListeners();
-    }
-
-    // Function to add event listeners to pagination links
-    function addPaginationListeners() {
-        const paginationLinks = pagination.querySelectorAll('a');
-        paginationLinks.forEach(link => {
-            link.addEventListener('click', function(e) {
-                e.preventDefault();
-                currentPage = parseInt(this.getAttribute('data-page'));
-                displayRecipes();
-            });
-        });
-    }
-
-    // Function to display recipes for the current page
-    function displayRecipes() {
-        const recipes = document.getElementsByClassName('recipe');
-        const startIndex = (currentPage - 1) * recipesPerPage;
-        const endIndex = startIndex + recipesPerPage;
-
-        for (let i = 0; i < recipes.length; i++) {
-            if (i >= startIndex && i < endIndex) {
-                recipes[i].style.display = "";
-            } else {
-                recipes[i].style.display = "none";
-            }
-        }
-
-        updatePagination(recipes.length);
-    }
-
-    // Add event listeners
-    searchInput.addEventListener('keyup', searchRecipes);
-
-    const sortOptions = document.querySelectorAll('.option');
-    const sortIndicator = document.querySelector('.sort-indicator');
-    let currentSortBy = 'name';
-    let currentOrder = 'asc';
-
-    sortOptions.forEach(option => {
-        option.addEventListener('click', function(e) {
-            e.preventDefault();
-            const newSortBy = this.getAttribute('data-sort');
-            
-            if (newSortBy === currentSortBy) {
-                currentOrder = currentOrder === 'asc' ? 'desc' : 'asc';
-            } else {
-                currentOrder = 'asc';
-                currentSortBy = newSortBy;
-            }
-
-            sortIndicator.setAttribute('data-order', currentOrder);
-            sortIndicator.querySelector('.asc-arrow').style.fill = currentOrder === 'asc' ? '#A19A9A' : '#E0E0E0';
-            sortIndicator.querySelector('.desc-arrow').style.fill = currentOrder === 'desc' ? '#A19A9A' : '#E0E0E0';
-
-            sortRecipes(currentSortBy, currentOrder);
-            sortOptions.forEach(opt => opt.classList.remove('active'));
-            this.classList.add('active');
-        });
-    });
-
-    // Initial display
-    displayRecipes();
-});
-  </script>
+  </div>
 
   <footer>
     <p>© <script>document.write(new Date().getFullYear())</script> Euan Steven</p>
