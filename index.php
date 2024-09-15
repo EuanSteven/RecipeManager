b177306d5989a9f44faa0e3a3db715c68042dafc
EuanSteven <23006327@uhi.ac.uk>
2024-09-15 11:35:04 +0100

    REMOVE

--- index.php
+++ index.php
@@ -52,12 +52,6 @@
       </svg>
       <a href="/pages/import.php" class="recipe_buttons">Import</a>
     </button>
-    <button class="w3-btn w3-white w3-border w3-border-green w3-round-xlarge">
-      <svg xmlns="http://www.w3.org/2000/svg" height="15px" viewBox="0 -960 960 960" width="20px" fill="#a19a9a">
-        <path d="M96-720v-192h192v72H168v120H96Zm696 0v-120H672v-72h192v192h-72ZM96-48v-192h72v120h120v72H96Zm576 0v-72h120v-120h72v192H672ZM312-264h336v-432H312v432Zm-72 72v-576h480v576H240Zm144-360h192v-72H384v72Zm0 108h192v-72H384v72Zm0 108h192v-72H384v72Zm-72 72v-432 432Z"/>
-      </svg>
-      <a href="/pages/scan.php" class="recipe_buttons">Scan</a>
-    </button>
   </div>
   
   <div class="search-container">
--- pages/import.php
+++ pages/import.php
@@ -34,7 +34,7 @@
       <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#A19a9a">
         <path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z"/>
       </svg>
-      <span><a class="back" href="/manager.html">Back</a></span>
+      <span><a class="back" href="/index.php">Back</a></span>
     </div>
   </div>
 
