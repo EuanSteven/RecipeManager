09326fa72812c701a72f82dc7f63b381579cba2d
Euan Steven <23006327@uhi.ac.uk>
2024-10-03 09:32:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
   $conn = new mysqli($servername, $username, $password, $dbname);
 
   if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
+      die("Connection Failed: " . $conn->connect_error);
   }
 
   // Fetch recipes from database
